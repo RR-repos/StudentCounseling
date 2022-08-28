21ee630ae1929153399d6859af1cc399d8a89930
Ravali <83853899+Void4413@users.noreply.github.com>
2022-08-28 17:16:05 +0530

    ques.py instructions and design model

--- ques.py
+++ ques.py
@@ -1,2 +1,4 @@
 #calculate different scores, check scoring scheme- https://docs.google.com/document/d/1BKUZlRhjtckP8GE7OKepq_7KMr8xX5p483xrNK2SeeM/edit
 #for the questions- https://docs.google.com/spreadsheets/d/1nXtuT1f3q4WXAySqABIigc0tZGIFY8_jz6fiUTWZ68w/edit#gid=0
+#check out https://www.betterhelp.com/get-started/?skip_redirect_question=1 for the working model, our questionaire page shud be like that, 
+#along with the user being able to navigate between questions
